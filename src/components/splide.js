a62c15587c8fb8327a22f6f82f969892fc84388d
Daan Roelofs <daanroelofs599@gmail.com>
2021-03-16 19:15:30 +0100

    mobile responsivess

--- src/components/splide.js
+++ src/components/splide.js
@@ -1,28 +1,35 @@
-import Splide from '@splidejs/splide'
+import Splide from '@splidejs/splide';
+import Video from '@splidejs/splide-extension-video';
+
 
 export const splide = () => {
   if(window.innerHeight > window.innerWidth) {
     // portrait
     new Splide( '.splide', {
-      type       : 'fade',
-      arrows     : false,
-      cover      : true,
-      height     : '100vh'
-    }).mount()
+        type       : 'fade',
+        arrows     : false,
+        cover      : true,
+        height     : '100vh'
+  
+    }).mount( { Video } )
   } else {
     // landscape
     new Splide( '.splide', {
-      type       : 'loop',
-      padding    : {
-        right    : '10rem',
-        left     : '10rem'
+        video      : {
+        width: '100vw',
       },
-      gap        : '3rem',
-      height     : '60vh',
-      cover      : true,
-      width      : '100vw',
-      focus      : 'center',
-      trimSpace  : true
-    }).mount()
+        type       : 'loop',
+        padding    : {
+          right    : '10rem',
+          left     : '10rem'
+        },
+        gap        : '3rem',
+        height     : '60vh',
+        cover      : true,
+        width      : '100vw',
+        focus      : 'center',
+        trimSpace  : true,
+        pagination : false
+    }).mount( { Video } );
   }
 }
